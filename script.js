5a3fce5ae14eab3bc3394e4ccd47bc1752e0e571
Azhwake <azhwaq2002@gmail.com>
2024-09-21 22:22:50 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Good Luck 💪🏾
+function reverseWords(str) {
+    return str.split(' ').reverse().join(' ');
+}
+
+
+console.log(reverseWords("fikrcamp bootcamp"));  // ➞ "bootcamp fikrcamp"
+console.log(reverseWords("hello there!"));       // ➞ "there! hello"
+console.log(reverseWords("welcome to my city")); // ➞ "city my to welcome"
+
